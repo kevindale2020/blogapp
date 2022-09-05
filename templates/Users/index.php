76fecd5a20e3eeb553eb43d05a4c2efbe7edcdd1
kevindale2020 <kevindale2017@gmail.om>
2022-09-05 21:10:00 +0800

    Update index.php

--- templates/Users/index.php
+++ templates/Users/index.php
@@ -14,7 +14,7 @@
       
         <!-- Modal Header -->
         <div class="modal-header">
-          <h4 class="modal-title">Add Post Here</h4>
+          <h4 class="modal-title">Add Post</h4>
           <button type="button" class="close" data-dismiss="modal">&times;</button>
         </div>
         
